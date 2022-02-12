ac6b96dfb5fe0a2635c4264c35cb7c9b2e40a9b3
keatonconrad <keaton.conrad@gmail.com>
2022-02-12 14:04:50 -0600

    Consolidates into one .py file

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A bot that plays a Wordle clone. This bot plays a slight variation, only using the possible answers, not the guessable words. Additionally, it plays on "hard mode," meaning yellow and green letters must appear in the next guess.
 
-After playing 50,000 games, it able to correctly guess the word within 6 tries 95.1% of the time.
-It takes an average of 4.47 guesses to solve the word.
+After playing 50,000 games, it able to correctly guess the word within 6 tries 95% of the time.
+It takes an average of 4.48 guesses to solve the word.
 
 After running a trial through each possible word, starting with SALON resulted in the fewest average guesses to solve, at 4.38.
--- wordlebot.py
+++ wordlebot.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import trange
 from statistics import mean
+from wordfreq import word_frequency
+from operator import itemgetter
 
 class LetterResult(Enum):
     GREEN = 0
@@ -12,30 +14,42 @@ class LetterResult(Enum):
     BLACK = 2
 
 
-def get_all_words():
-    with open('all_words.txt', 'r') as f:
-        return [line.strip() for line in f.readlines()]
+with open('all_words.txt', 'r') as f:
+    all_words = [line.strip() for line in f.readlines()]
 
 
 class WordleBot:
 
-    def __init__(self):
-        self.all_words = get_all_words()
+    def __init__(self, first_word=None, use_frequencies=True):
+        self.all_words = all_words
         self.scores = []
         self.answer = ''
         self.last_score = []
         self.guesses = []
         self.won = False
         self.logging = False
+        self.first_word = first_word  # Word to start with
+        self.use_frequencies = use_frequencies  # Take into account word frequencies when guessing
         
     
     def play(self):
         self.answer = random.choice(self.all_words)
 
         while True:
-            candidates = self.get_candidates()
-            guess = random.choice(candidates)
+            if len(self.guesses) == 0 and self.first_word:
+                guess = self.first_word
+            else:
+                candidates = self.get_candidates()
+
+                if self.use_frequencies:
+                    frequencies = [(word, word_frequency(word, 'en')) for word in candidates]
+                    frequencies.sort(key=itemgetter(1), reverse=True)
+                    guess = frequencies[0][0]
+                else:
+                    guess = random.choice(self.all_words)
+
             score = self.score_guess(guess)
+
             if all([result[2] == LetterResult.GREEN for result in score]):
                 if len(self.guesses) <= 6:
                     self.won = True
@@ -105,21 +119,22 @@ class WordleBot:
 
 
 class Tester:
-    def __init__(self, num_trials=100):
+    def __init__(self, num_trials=100, first_word=None):
         self.win_counter = 0  # Number of times correct word was guessed within 6 tries
         self.num_guesses = []  # Array of how many tries it took to guess each word
         self.num_trials = num_trials
+        self.first_word = first_word
 
     def play_game(self):
         for i in trange(self.num_trials):
-            w = WordleBot()
+            w = WordleBot(self.first_word)
             w.play()
             self.num_guesses.append(len(w.guesses))
             if w.won:
                 self.win_counter += 1
 
 if __name__ == '__main__':
-    w = Tester(num_trials=50000)
+    w = Tester(num_trials=len(all_words))
     w.play_game()
     print(f'% won within 6 guesses: {w.win_counter / w.num_trials}')
     print(f'Average num. guesses: {mean(w.num_guesses)}')
